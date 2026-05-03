bcde0c65a8a2f0ca42280fcc62144099bb61f4c4
y.fahmi <y.fahmi@karizma.ma>
2026-05-03 18:56:17 +0100

    [UPD]

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
+import { useRef } from "react";
 import './App.css'
 import { Countdown } from './components/Countdown'
 import Welcome from './components/Welcome'
 
 function App() {
+  const audioRef = useRef(null);
+
+  const startMusic = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
 
   return (
     <>
+      <audio ref={audioRef} loop>
+        <source src="/music.mp3" type="audio/mpeg" />
+      </audio>
+
       {/* Hero */}
-      <div className='hero'>
-        {/* Couple video on the background */}
-        {/* Text on the center */}
+      <div onClick={startMusic}>
+        <div className='hero'>
+          {/* Couple video on the background */}
+          {/* Text on the center */}
+        </div>
+        {/* Welcome */}
+        <Welcome />
+
+        {/* contdown */}
+        <Countdown />
+
+
+        {/* location */}
       </div>
-      {/* Welcome */}
-      <Welcome />
-
-      {/* contdown */}
-      <Countdown />
-
-
-      {/* location */}
     </>
   )
 }
