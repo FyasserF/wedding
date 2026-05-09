e4f6bb30ed1e416a3184c844e73e2e8607293bb0
y.fahmi <y.fahmi@karizma.ma>
2026-05-09 15:36:52 +0100

    [UPD]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useEffect, useRef, useState } from "react";
-import './App.css'
-import { Countdown } from './components/Countdown'
-import Welcome from './components/Welcome'
+import { useRef, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./App.css";
+import Welcome from "./components/Welcome";
+import { Countdown } from "./components/Countdown";
 import Location from "./components/Location";
 import { IoMdMusicalNote } from "react-icons/io";
 
-function App() {
+export default function App() {
+
+  const [started, setStarted] = useState(false);
+  const [showLanding, setShowLanding] = useState(false);
   const audioRef = useRef(null);
   const buttonRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleOpen = async () => {
+    setStarted(true);
+    toggleMusic()
+    setTimeout(() => {
+      setShowLanding(true);
+    }, 2000);
+  };
 
   const fadeIn = (audio) => {
     audio.volume = 0;
@@ -52,29 +64,91 @@ function App() {
     setIsPlaying(!isPlaying);
   };
 
-
   return (
     <>
-      {/* Audio */}
       <audio ref={audioRef} loop>
         <source src="/music.mp4" type="audio/mpeg" />
       </audio>
 
-      {/* Hero */}
-      <div>
-        <div className='hero'></div>
+      <AnimatePresence>
+        {!started && (
+          <motion.div
+            className="intro-content"
+            initial={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
 
-        <Welcome />
-        <Countdown />
-        <Location />
-      </div>
+            <section className="hero">
+              <div className="hero-bg">
+                <img src='/hero-couple.jpg' />
+              </div>
 
-      {/* Floating Music Button */}
-      <button ref={buttonRef} className="music-btn" onClick={toggleMusic}>
-        <IoMdMusicalNote />
-      </button>
+              <div className="hero-overlay" />
+              <div className="hero-blur" />
+
+              <div className="hero-content">
+                <motion.p
+                  className="hero-subtitle"
+                  initial={{ opacity: 0, y: 30 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: .1 }}
+                >
+                  Nous avons la joie de vous convier à notre mariage
+                </motion.p>
+
+                <h1 className="hero-title">
+                  Majda <span>&</span> Moncef
+                </h1>
+
+                <motion.button
+                  className="open-btn"
+                  onClick={handleOpen}
+                  whileHover={{
+                    scale: 1.05,
+                    boxShadow: "0 0 30px rgba(255,255,255,0.3)"
+                  }}
+                  whileTap={{ scale: 0.96 }}
+                >
+                  Découvrir l’invitation
+                </motion.button>
+              </div>
+            </section>
+
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      <motion.div
+        className="black-screen"
+        animate={{
+          opacity: started && !showLanding ? 1 : 0
+        }}
+        transition={{
+          duration: 2,
+          delay: 0.4
+        }}
+      />
+
+      <AnimatePresence>
+        {showLanding && (
+          <motion.div
+            // className="landing-page"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5 }}
+          >
+            <div className="hero" />
+
+            <Welcome />
+            <Countdown />
+            <Location />
+            {/* Floating Music Button */}
+            <button ref={buttonRef} className="music-btn" onClick={toggleMusic}>
+              <IoMdMusicalNote />
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
-  )
+  );
 }
-
-export default App;
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -5,12 +5,12 @@ export default function Location() {
         <div className='location'>
             <h2>Où célébrons-nous ?</h2>
             <div className="card">
-                <h3>Casablanca el hay hasssani rue 2</h3>
+                <h3>Palais KZY</h3>
                 <h3>17h00</h3>
 
                 <div className="map">
                     <iframe
-                        src="https://www.google.com/maps?q=Casablanca+El+Hay+Hassani+Rue+2&output=embed"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3327.9942030010043!2d-7.745205400000002!3d33.4755017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda62bade226ceb3%3A0xb96e59454f6f7c54!2spalais%20kzy!5e0!3m2!1sfr!2sma!4v1778280323076!5m2!1sfr!2sma"
                         width="100%"
                         height="100%"
                         style={{ border: 0 }}
